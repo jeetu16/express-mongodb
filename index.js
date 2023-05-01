ecc09cdec52abc41f63be390cd51494bdb23b978
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-05-01 15:51:14 +0530

    express mongodb backend app

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// db connection
+
+
+main().catch(err => console.log(err))
+
+async function main() {
+    await mongoose.connect("mongodb://localhost:27017/ecommerce");
+    //mongodb://localhost:27017/
+    console.log("Database connected...");
+}
+
+
+
+// built-in middlewares
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// routes
+app.use('/users',require('./routes/user.routes'));
+app.use('/products',require('./routes/product.routes'))
+
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
+})
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Express with mongodb
+
+### In this repo i've learn about database mongodb.
+
+### Mongodb is NoSQL database. In mongodb we have databases, databases has collections(like table in SQL) and those collections have documents(like row in SQL).
